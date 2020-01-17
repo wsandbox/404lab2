9fff0c5a5d13e205db2351c5c9c24a509b9453c5
wsandbox <warrenssandbox@gmail.com>
2020-01-17 00:43:40 -0500

    proxy client/server working

--- proxy_client.py
+++ proxy_client.py
@@ -1,3 +1,17 @@
 import socket
 
-sclient = socket.create_connection(
+HOST = 'localhost'
+PORT = 8001
+header = 'GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n'
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST,PORT))
+    sock.sendall(header.encode())
+    sock.shutdown(socket.SHUT_WR)
+    message = b""
+    data = sock.recv(4096)
+    while data:
+        message += data
+        data = sock.recv(4096)
+    print(message.decode('ISO-8859-1'))
+
+    
--- proxy_server.py
+++ proxy_server.py
@@ -1,14 +1,38 @@
-import socket, requests
+import socket, threading
 
 HOST = 'www.google.com'
 PORT = 80
 
-sock2 = socket.create_connection((HOST, PORT))
-print(sock2)
+HOST2 = 'localhost'
+PORT2 = 8001
 
-#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
- #   c, a = sock.create_connection((HOST, PORT))
-  #  print(c)
-    
+def handle(client):
+    with client, socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy:
+        proxy.connect((HOST, PORT))
+        data = client.recv(4096)
+        proxy.sendall(data)
+        proxy.shutdown(socket.SHUT_WR)
+
+        resp = proxy.recv(4096)
+        end_data = b""
+        while resp:
+            end_data += resp
+            resp = proxy.recv(4096)
+
+        client.sendall(end_data)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listener:
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listener.bind((HOST2, PORT2))
+    listener.listen()
+    while True:
+        c, a = listener.accept()
+        #print(c, a)
+        
+        stream = threading.Thread(target=handle, args=(c,))
+        stream.run()
+
+if __name__=='__main__':
+    main()
 
 
